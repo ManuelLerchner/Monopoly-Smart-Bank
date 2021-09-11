78b61c445043fdb76b42ee490115a31a9843328f
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2021-09-11 23:17:12 +0200

    add static

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var app = express();
-PORT=3000
+PORT = 3000;
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 
+app.use(express.static("public"));
+
+//Routes
 app.get("/", function (req, res) {
     res.render("home");
+});
+
+app.get("/test", function (req, res) {
+    res.render("test");
 });
 
 app.listen(PORT, () => {
